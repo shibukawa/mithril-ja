3d6f3f7564c5775da6f0769eb3a6898f5ad8d1e2
Yoshiki Shibukawa <shibukawa.yoshiki@dena.jp>
2017-02-22 08:15:05 +0900

    チュートリアルまで完了/sourceを1.0.1に更新

--- copy.sh
+++ copy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+rm -rf source
+mkdir -p source/archive/
+mkdir -p source/lib/
 cp ../mithril/*.html source/
-cp ../mithril/mithril* source/
-cp ../mithril/*.json source/
 cp ../mithril/*.css source/
 cp ../mithril/*.svg source/
 cp -r ../mithril/lib/* ./source/lib/
+cp -r ../mithril/archive/v0.2.5 source/archive/
 
