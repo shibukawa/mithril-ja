986a575dc33cb08f15d7f60b57bbcc60b7448460
Yoshiki Shibukawa <shibukawa.yoshiki@dena.jp>
2017-04-01 02:34:56 +0900

    1.1.0ベースにして翻訳

--- copy.sh
+++ copy.sh
@@ -2,10 +2,8 @@
 
 rm -rf source
 mkdir -p source/archive/
-mkdir -p source/lib/
-cp ../mithril/*.html source/
-cp ../mithril/*.css source/
-cp ../mithril/*.svg source/
-cp -r ../mithril/lib/* ./source/lib/
-cp -r ../mithril/archive/v0.2.5 source/archive/
+cp ../mithril.js/*.html source/
+cp ../mithril.js/*.css source/
+cp ../mithril.js/*.svg source/
+cp -r ../mithril.js/archive/v0.2.5 source/archive/
 
